d4043fa30117ce90d2b2dc4011b57ce87703e72e
dayana <dayanasavickaya@gmail.com>
2025-01-03 00:25:46 +0300

    3.1.2 Spring Boot, Security

---
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import ru.kata.spring.boot_security.demo.util.UserCreator;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public User findUserById(long id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(long id, User user) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUsersByUsername(username);
+    }
+
+    @PostConstruct
+    private void createUsers() {
+        UserCreator.createUsersWithRoles().forEach(this::saveUser);
+    }
+
+
+    @Override
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
+}
